d95fd0dc9f091d45e74a744219dcb8566a0c9961
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-10-28 00:31:27 +0500

    bugs fixed

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        float diff = numberToBeRounded - (int) numberToBeRounded;
+        if (diff >= 0.5) {
+            System.out.println(Math.ceil(numberToBeRounded));
+        } else {
+            System.out.println(Math.floor(numberToBeRounded));
+
+        }
     }
 }
+
